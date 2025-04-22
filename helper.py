b5eb47acd9ed8629772aa33dc19125ea8ee306bb
Gosh Kolotyan <goshkolotyan@gmail.com>
2025-04-22 18:42:02 +0400

    adding encoder need to be added features tensor

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
 import re 
 
+
+
 def parse_compostion(composition:str) -> list[tuple[str, str]]:
     pattern = r'([A-Z][a-z]*)([0-9]*\.?[0-9]*)'
     matches = re.findall(pattern, composition)
@@ -7,11 +9,33 @@ def parse_compostion(composition:str) -> list[tuple[str, str]]:
     elements_fractions = []
     for element, fraction in matches:
         frac = float(fraction)  # add logic for missing fractions
+        if frac > 1:
+            frac = frac / 100.0
+        elif frac < 1 and frac > 0:
+            frac = frac
         elements_fractions.append((element, frac))
+    
+    while len(elements_fractions) < 4:
+        elements_fractions.append(("", 0.0))
     return elements_fractions
-
 #example usage
 
-composition = "Sn14.0Bi35.9In50.1"
-elements_fractions = parse_compostion(composition)
-print(elements_fractions)
+# composition = "Sn14.0Bi35.9In50.1"
+# elements_fractions = parse_compostion(composition)
+# print(elements_fractions)
+import math
+
+def calculate_entropy_of_mixing(concentrations):
+    pass
+
+def calculate_mix_enthalpy():
+    pass
+def calculate_eletronegativity_difference():
+    pass
+def calculate_atomic_radius_difference():
+    pass
+def calculate_melting_point():
+    pass
+
+
+
--- tokenizer.py
+++ tokenizer.py
@@ -1,16 +1,13 @@
-import re
+import json
 import torch
-import pandas as pd
-import torch.nn as nn
-import torch.nn.functional as F
 from helper import parse_compostion
-
 
 
 class LM_Tokenizer:
     def __init__(self):
-        self.vocab = None
+        self.vocab = json.load(open("elements_vocab.json"))
         self.vocab_size = len(self.vocab)
+
     
 
     def encode(self, composition:str, target:float) -> torch.Tensor:
@@ -19,6 +16,25 @@ class LM_Tokenizer:
         output must be a tensot size of (4, n) where n is number of components
         """
         elements_fractions = parse_compostion(composition)
+        output = []
+        for element, fraction in elements_fractions:
+            output.append([
+                self.vocab[element]['atomic_number'] if element in self.vocab else 0,
+                fraction,
+                self.vocab[element]["atomic radius"] if element in self.vocab else 0,
+                self.vocab[element]['electronegativity'] if element in self.vocab else 0,
+                self.vocab[element]['melting_point'] if element in self.vocab else 0,
+            ])
+        return torch.tensor(output, dtype=torch.float64), \
+               torch.tensor([target], dtype=torch.float64)
+
+LM_Tokenizer = LM_Tokenizer()
+composition , target = LM_Tokenizer.encode("Sn14.0Bi35.9In50.1", 0.1)
+print(composition)
+print(composition.shape)
+print(target.item())
+
+
 
 
 
